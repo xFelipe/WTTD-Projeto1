e8985c020ebfbfa68b4f5da922cfa00ec82e1330
xFelipe <felipegdmito@gmail.com>
2019-06-12 01:56:58 -0300

    Tentando resolver errode push

--- eventex/settings.py
+++ eventex/settings.py
@@ -126,7 +126,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # E-mail configuration
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_BACKEND = config('EMAIL_BACKEND')
 EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = config('EMAIL_PORT', cast=int)
