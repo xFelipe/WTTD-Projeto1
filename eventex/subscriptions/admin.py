bc9a3bd27b11661e02035371537cc4d038a81af4
xFelipe <felipegdmito@gmail.com>
2019-07-03 04:26:50 -0300

    Mensagem adicionada ao marcar inscrições como pagas

--- eventex/subscriptions/admin.py
+++ eventex/subscriptions/admin.py
@@ -10,12 +10,26 @@ class SubscriptionModelAdmin(admin.ModelAdmin):
     search_fields = ('name', 'email', 'phone', 'cpf', 'created_at')  # Prioridade da busca
     list_filter = ('paid', 'created_at')
 
+    actions = ['mark_as_paid']
+
     def subscribed_today(self, obj):
         return obj.created_at == now().date()
 
     subscribed_today.short_description = 'inscrito hoje?'
 
     subscribed_today.boolean = True
+
+    def mark_as_paid(self, request, queryset):
+        count = queryset.update(paid=True)
+
+        if count == 1:
+            msg = '{} inscrição foi marcada como paga.'
+        else:
+            msg = '{} inscrições foram marcadas como pagas.'
+
+        self.message_user(request, msg.format(count))
+
+    mark_as_paid.short_description = 'Marcar como pago'
 
 
 admin.site.register(Subscription, SubscriptionModelAdmin)
