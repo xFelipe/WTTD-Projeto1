efeefc6c275a01e6e143baf82a0cced2d18c163d
xFelipe <felipegdmito@gmail.com>
2019-07-04 00:46:23 -0300

    Padroniza nomes

--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -20,6 +20,12 @@ class SubscriptionFormTest(TestCase):
         form = self.make_validated_form(cpf='1234')
         self.assertFormErrorCode(form, 'cpf', 'lenght')
 
+    def test_name_must_be_captalized(self):
+        """ Name must be captalized. """
+        # FELIPE gomes -> Felipe Gomes
+        form = self.make_validated_form(name='FELIPE gomes')
+        self.assertEqual('Felipe Gomes', form.cleaned_data['name'])
+
     #
 
     def assertFormErrorCode(self, form, field, code):
